5fa965119a96a8a8a6462d3d32bf4b141965995d
Nick Humrich <nick.humrich@gmail.com>
2016-08-22 17:09:15 -0600

    Add mock cursor

--- asyncpgsa/testing/mockpreparedstmt.py
+++ asyncpgsa/testing/mockpreparedstmt.py
@@ -7,9 +7,22 @@ class MockPreparedStatement:
         self._state = state
 
     def cursor(self, *args, **kwargs):
-        return self._connection.results.get_nowait()
+        return MockCursor(self._connection.results.get_nowait())
 
     def __getattr__(self, item):
         raise NotImplementedError('Sorry, this doesnt exist yet. '
                                   'Consider making a PR.')
+
+
+class MockCursor:
+    def __init__(self, list):
+        self.iterator = iter(list)
+
+    async def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        return next(list)
+
+
 
