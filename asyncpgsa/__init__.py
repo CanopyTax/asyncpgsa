a11d1efa18c03d93942d95ddaa58c079a56716a7
Choi Geonu <6566gun@gmail.com>
2018-01-13 15:06:59 +0900

    Add __all__ to __init__.py

--- asyncpgsa/__init__.py
+++ asyncpgsa/__init__.py
@@ -4,3 +4,12 @@ from .connection import compile_query
 from .version import __version__
 
 pg = PG()
+
+
+__all__ = [
+    'create_pool',
+    'PG',
+    'compile_query',
+    '__version__',
+    'pg',
+]
