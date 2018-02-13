14ec1b30fd537afea200f28ae1e7cd637c77e8ae
Nick Humrich <nick.humrich@gmail.com>
2018-02-13 09:25:36 -0700

    (hopefully) Fix pool.transaction to always close connection

--- asyncpgsa/transactionmanager.py
+++ asyncpgsa/transactionmanager.py
@@ -1,3 +1,4 @@
+import asyncio
 
 
 class ConnectionTransactionContextManager:
@@ -35,5 +36,10 @@ class ConnectionTransactionContextManager:
         return con
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.transaction.__aexit__(exc_type, exc_val, exc_tb)
-        await self.acquire_context.__aexit__(exc_type, exc_val, exc_tb)
+        async def _close():
+            try:
+                await self.transaction.__aexit__(exc_type, exc_val, exc_tb)
+            finally:
+                await self.acquire_context.__aexit__(exc_type, exc_val, exc_tb)
+
+        await asyncio.shield(_close())
